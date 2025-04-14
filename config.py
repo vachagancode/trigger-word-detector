c350d0fe5e00df0eb55cb3803112afb7c42237cf
Vach <karapetyanvachbuisness@gmail.com>
2025-04-14 21:42:58 +0400

    change loss function and remove the <background> label

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ def get_config():
         "in_features" : 39,
         "conv_out_features" : 64,
         "hidden_size" : 512,
-        "out_features": 3,
+        "out_features": 2,
         "num_layers" : 2,
         "dropout": 0.5,
 
-        "epochs" : 20
+        "epochs" : 30
     }
